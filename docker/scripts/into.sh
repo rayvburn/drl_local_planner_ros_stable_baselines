910f6b1ed71210f8173362e7089e90643a1fa673
Stepan <steplerchik@gmail.com>
2022-11-27 21:39:17 +0300

    build from host ws

--- docker/scripts/into.sh
+++ docker/scripts/into.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker exec -it drl_local_planner \
-    /bin/bash -c "cd ..; source devel/setup.bash; cd src; /bin/bash"
+    /bin/bash -c "source catkin_ws/devel/setup.bash; /bin/bash"
