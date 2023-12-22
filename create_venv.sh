ab2fdd3c29fd71912ac78b76ee19b896938fe881
Jarek Karwowski <chromedivizer@gmail.com>
2023-12-22 15:00:47 +0100

    `create_venv` script extended to be more robust
    These changes are required on the TIAGo's computer

--- create_venv.sh
+++ create_venv.sh
@@ -10,6 +10,12 @@ source .venv/bin/activate
 
 pip3 install -r requirements.txt
 
+# Extracted from the requirements.txt as on some platforms this error may show up:
+#   ERROR: Could not find a version that satisfies the requirement pkg-resources==0.0.0 (from versions: none)
+#   ERROR: No matching distribution found for pkg-resources==0.0.0
+# which breaks the whole installation
+pip3 install pkg-resources==0.0.0
+
 # prepare rl_agent sources
 cd $SCRIPT_DIR/rl_agent
 python setup.py install
